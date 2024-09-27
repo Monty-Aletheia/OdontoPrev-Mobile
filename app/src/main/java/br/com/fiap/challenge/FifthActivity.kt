1ac36e9a3942fe2ff3632fce8c0c84449dea3fac
Pedro Lucca <pedroluccaprofissional@gmail.com>
2024-09-27 09:32:23 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/challenge/FifthActivity.kt
@@ -0,0 +1,28 @@
+package br.com.fiap.challenge
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FifthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fifth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val previousArrow = findViewById<ImageButton>(R.id.previousArrow)
+        previousArrow.setOnClickListener {
+            val intent = Intent(this, FourthActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
