6848654509d987a41ead16607646c02875674afc
Pedro Lucca <pedroluccaprofissional@gmail.com>
2024-10-04 10:23:36 -0300

    refactor: refactoring

--- app/src/main/java/br/com/fiap/challenge/ui/login/LoginActivity.kt
+++ app/src/main/java/br/com/fiap/challenge/ui/login/LoginActivity.kt
@@ -1,27 +1,22 @@
-package br.com.fiap.challenge
+package br.com.fiap.challenge.ui.login
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import br.com.fiap.challenge.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val button = findViewById<Button>(R.id.button)
-
 
 
     }
