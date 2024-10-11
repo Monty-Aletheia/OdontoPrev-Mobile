7d96678ba8de6dd20b202552e3ef4cd6520de4e4
Pedro Lucca <pedroluccaprofissional@gmail.com>
2024-10-11 10:39:21 -0300

    feat: adding menu footer

--- app/src/main/java/br/com/fiap/challenge/ui/login/AppMenuActivity.kt
+++ app/src/main/java/br/com/fiap/challenge/ui/login/AppMenuActivity.kt
@@ -7,17 +7,16 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.com.fiap.challenge.R
 
-class LoginActivity : AppCompatActivity() {
+class AppMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_app_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
 
 
 
---
+++ app/src/main/java/br/com/fiap/challenge/ui/login/ProfileFragment.kt
@@ -0,0 +1,21 @@
+package br.com.fiap.challenge.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.fiap.challenge.R
+
+class ProfileFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
